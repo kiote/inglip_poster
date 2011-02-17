aac5131690f8dc77ccb98bea0d805494ff94117d
Krivich Ekaterina <krivich.ekaterina@gmail.com>
2011-02-17 19:42:01 +0300

    yaml params added

--- serialmaniak/poster.rb
+++ serialmaniak/poster.rb
@@ -1,9 +1,13 @@
 $KCODE = "U"
 require 'sqlite3'
+require 'pp'
 
 base = File.dirname(__FILE__)
 
 require "#{base}/robot"
+require 'yaml'
+
+yml = YAML.load_file "#{base}/config.yml" 
 
 PHRASE_GETTER = lambda do
   db = SQLite3::Database.new "#{base}/maniak.db"
@@ -11,7 +15,7 @@ PHRASE_GETTER = lambda do
   until (!text.nil? and text.length < 121)
     row = db.get_first_row "select * from quotes \
 where id not in (select id from posted)\
-ORDER BY RANDOM() "
+ORDER BY RANDOM() #"
     fail 'no texts found' if row.nil?
     id, text = row[0], row[2]
     db.execute_batch "insert into posted (id) values (#{id})"
@@ -19,10 +23,10 @@ ORDER BY RANDOM() "
   text
 end
 
-client = Robot.new({:consumer_key => 'qhQYAKHy5mVWhdqPD0DX1g',
-  :consumer_secret => 'bFgbeWrr6y10qtFrC089WswSgbhWdmQPYZxbvB30uwk',
-  :oauth_token => '27295510-cZOZYtSnRHYEdnLyIcEbWPFaDQz0Ev7RdZanUbZY',
-  :oauth_token_secret => '6bIrZX6burEjHS5BMrYcQDBFKvtK2lsr4E2sOcnOQYg'}, &PHRASE_GETTER)
+client = Robot.new({:consumer_key => yml['serialmaniak']['consumer_key'],
+  :consumer_secret => yml['serialmaniak']['consumer_secret'],
+  :oauth_token => yml['serialmaniak']['oauth_token'],
+  :oauth_token_secret => yml['serialmaniak']['oauth_token_secret']}, &PHRASE_GETTER)
 
 begin
   client.follow_all_back
