d55399453913ff86deee3e5cd2336dfd4c97e87e
Krivich Ekaterina <krivich.ekaterina@gmail.com>
2011-03-23 17:42:36 +0300

    require

--- wanna/follower.rb
+++ wanna/follower.rb
@@ -1,7 +1,7 @@
 $KCODE = "U"
-require 'lib/robot'
-
 base = File.dirname(__FILE__)
+
+require './' + base + '/../lib/robot'
 
 Robot::Path.set_base(base)
 client = Robot::Robot.new "#{base}/config.yml", 'wanna'
