f0ea02906db706c60fb01da8c203b475ff9f4336
apps <apps@vs1191219.vserver.de>
2011-03-24 10:04:27 +0000

    fix require

--- wanna/retweet.rb
+++ wanna/retweet.rb
@@ -1,7 +1,7 @@
 $KCODE = "U"
 base = File.dirname(__FILE__)
 
-require './' + base + '/../lib/robot'
+require base + '/../lib/robot'
 
 Robot::Path.set_base(base)
 client = Robot::Robot.new "#{base}/config.yml", 'wanna'
