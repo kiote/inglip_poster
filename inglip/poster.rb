ea3a230b3cd02a4120e26e948550e9422bcd5344
krivich ekaterina <kiote_the_one@mail.ru>
2012-03-21 20:43:45 +0400

    fixed for new robotwitter gem

--- inglip/poster.rb
+++ inglip/poster.rb
@@ -1,20 +1,16 @@
-$KCODE = "U"
+# encoding: utf-8
 require 'rubygems'
 require 'robotwitter'
 
-Robotwitter::Path.set_base File.dirname(__FILE__)
-
-PHRASE_GETTER = lambda do
-    open("http://inglip.ru/api/claim") do |f|
-      f.each do |line|
-        @line = line
-      end
+client = Robotwitter::Robot.new(File.expand_path('..', __FILE__), 'inglip') do
+  open("http://inglip.ru/api/claim") do |f|
+    f.each do |line|
+      @line = line
     end
-
-    @line
+  end
+  @line
 end
 
-client = Robotwitter::Robot.new "config.yml", 'inglip' , &PHRASE_GETTER
 client.send_message "_msg_ #inglip"
 
 client.follow_all_back
